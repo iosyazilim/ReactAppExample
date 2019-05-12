dd2c758a18c6ecb72e136eea69ca55d72e051c62
Ercan Er <iosyazilim@gmail.com>
2019-05-12 21:16:43 +0300

    New React Update

--- src/container/App.js
+++ src/container/App.js
@@ -1,18 +1,34 @@
-import React, { Component } from "react";
-import classes from "./App.css";
-import Persons from "../components/Persons/Persons";
-import Cockpit from "../components/Cockpit/Cockpit";
+import React, { Component } from 'react';
+import classes from './App.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 class App extends Component {
-  state = {
-    persons: [
-      { id: "asfa1", name: "Max", age: 28 },
-      { id: "vasdf1", name: "Manu", age: 29 },
-      { id: "asdf11", name: "Stephanie", age: 26 }
-    ],
-    otherState: "some other value",
-    showPersons: false
-  };
+  constructor(props) {
+    super(props);
+    console.log('[App.js] consructor');
+    this.state = {
+      persons: [
+        { id: 'asfa1', name: 'Max', age: 28 },
+        { id: 'vasdf1', name: 'Manu', age: 29 },
+        { id: 'asdf11', name: 'Stephanie', age: 26 }
+      ],
+      otherState: 'some other value',
+      showPersons: false
+    };
+  }
 
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  // componentWillMount(){
+  //   console.log('[App.js] componentWillMount');
+  // }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
@@ -31,7 +47,6 @@ class App extends Component {
   };
 
   deletePersonHandler = personIndex => {
-  
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({ persons: persons });
@@ -43,6 +58,7 @@ class App extends Component {
   };
 
   render() {
+    console.log('[App.js] render');
     let persons = null;
 
     if (this.state.showPersons) {
